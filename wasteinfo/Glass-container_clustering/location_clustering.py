1e49a14838aa8f35dbe857a1816813c3725a22ff
zafariqbal1993 <72519457+zafariqbal1993@users.noreply.github.com>
2021-04-21 22:46:14 +0200

    added inline plot

--- wasteinfo/Glass-container_clustering/location_clustering.py
+++ wasteinfo/Glass-container_clustering/location_clustering.py
@@ -1,3 +1,4 @@
+%matplotlib inline
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns;
@@ -6,25 +7,12 @@ from sklearn.cluster import KMeans
 sns.set()
 
 df = pd.read_csv('location.csv')
-# df.head(10)
 
 df.dropna(axis=0, how='any', subset=['latitude', 'longitude'], inplace=True)
 
 # Variable with the Longitude and Latitude
 X = df.loc[:, ['id', 'latitude', 'longitude']]
-# X.head(10)
 
-K_clusters = range(1, 10)
-kmeans = [KMeans(n_clusters=i) for i in K_clusters]
-Y_axis = df[['latitude']]
-X_axis = df[['longitude']]
-score = [kmeans[i].fit(Y_axis).score(Y_axis) for i in range(len(kmeans))]
-# Visualize
-plt.plot(K_clusters, score)
-plt.xlabel('Number of Clusters')
-plt.ylabel('Score')
-plt.title('Elbow Curve')
-plt.show()
 
 kmeans = KMeans(n_clusters=12, init='k-means++')
 kmeans.fit(X[X.columns[1:3]])  # Compute k-means clustering.
